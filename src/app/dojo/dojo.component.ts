95352d3ff9f9899abdc74c4f3880056df1faafe3
bnymnDev <88560587+bnymnDev@users.noreply.github.com>
2023-01-14 01:02:01 +0100

    init material ui

--- src/app/dojo/dojo.component.ts
+++ src/app/dojo/dojo.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DojoComponent implements OnInit {
   inputString = "";
-  numRows =  3;
+  numRows = 3;
   result = "";
 
   convert(){
