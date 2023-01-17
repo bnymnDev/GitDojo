1f0fde5a0267d91a103833338b401fa02a5100d9
bnymnDev <88560587+bnymnDev@users.noreply.github.com>
2023-01-17 22:56:15 +0100

    test CodeSnippets

--- src/app/fizzbuzz/fizzbuzz.component.ts
+++ src/app/fizzbuzz/fizzbuzz.component.ts
@@ -20,6 +20,29 @@ export class FizzbuzzComponent implements OnInit {
   showSnippet: boolean = false
 
   fizzBuzzSnippet = this.generateFizzBuzz;
+  fizzBuzzSnippet2 =
+  `generateFizzBuzz() {
+    if (this.inputNumber <= 10000) {
+      this.fizzBuzzResults = [];
+      for (let i = 1; i <= this.inputNumber; i++) {
+        if (i % 15 === 0) {
+          this.fizzBuzzResults.push('FizzBuzz');
+        } else if (i % 3 === 0) {
+          this.fizzBuzzResults.push('Fizz');
+        } else if (i % 5 === 0) {
+          this.fizzBuzzResults.push('Buzz');
+        } else {
+          this.fizzBuzzResults.push(i);
+        }
+      }
+      this.dataSource.data = this.fizzBuzzResults;
+      this.snackBar.open('FizzBuzz successfull', 'OK', { duration: 1500 });
+    } else {
+      this.snackBar.open('FizzBuzz failed! Number is higher than 10000', 'OK', {
+        duration: 5000,
+      });
+    }
+  }`
 
   generateFizzBuzz() {
     if (this.inputNumber <= 10000) {
